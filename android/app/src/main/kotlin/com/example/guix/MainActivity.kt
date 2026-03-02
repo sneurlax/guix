b82fa5ae3811f155a623ed9f2253f8e06ee28176
sneurlax <sneurlax@gmail.com>
2026-03-02 17:11:34 -0600

    refactor: remove demo Flutter app scaffolding

--- android/app/src/main/kotlin/com/example/guix/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.guix
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:guix/main.dart';
-
-void main() {
-  testWidgets('app renders', (tester) async {
-    await tester.pumpWidget(const App());
-    expect(find.text('Flutter + Guix'), findsOneWidget);
-  });
-}
